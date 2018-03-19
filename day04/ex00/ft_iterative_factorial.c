75fa6a886193d9d374c55445d1769f8ab837b935
Po-ying Fu <pfu@e1z1r7p5.42.us.org>
2018-03-19 01:24:35 -0700

    Update day04

--- day04/ex00/ft_iterative_factorial.c
+++ day04/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: pfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 16:27:16 by pfu               #+#    #+#             */
-/*   Updated: 2018/03/17 16:33:33 by pfu              ###   ########.fr       */
+/*   Updated: 2018/03/19 01:19:55 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,24 @@ int		ft_iterative_factorial(int nb)
 
 	ans = 1;
 	i = 0;
-	while (i <= nb)
-	{
-		if (i == 0)
-		{
-			ans = ans * 1;
-		}
-		else
-		{
-			ans = ans * i;
-		}
-	}
-	if (ans == 0 || nb > 12)
+	if (nb < 0 || nb > 12)
 	{
 		return (0);
 	}
 	else
 	{
+		while (i <= nb)
+		{
+			if (i == 0)
+			{
+				ans = ans * 1;
+			}
+			else
+			{
+				ans = ans * i;
+			}
+			i++;
+		}
 		return (ans);
 	}
 }
--- day04/ex01/ft_recursive_factorial.c
+++ day04/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: pfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 18:05:36 by pfu               #+#    #+#             */
-/*   Updated: 2018/03/17 18:19:47 by pfu              ###   ########.fr       */
+/*   Updated: 2018/03/19 01:23:10 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ int		ft_recursive_factorial(int nb)
 	int ans;
 
 	i = 1;
-	if (nb <= 0)
+	if (nb < 0 || nb > 12)
+	{
+		return (0);
+	}
+	else if (nb == 0)
 	{
 		return (1);
 	}
@@ -24,12 +28,5 @@ int		ft_recursive_factorial(int nb)
 	{
 		i = nb * ft_recursive_factorial(nb - 1);
 	}
-	if (i == 0 || nb > 12)
-	{
-		return (0);
-	}
-	else
-	{
-		return (i);
-	}
+	return (i);
 }
