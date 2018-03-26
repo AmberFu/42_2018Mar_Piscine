1a34033785da1db2dcaf1902de5f428981ca1707
Po-ying Fu <pfu@e1z1r7p5.42.us.org>
2018-03-25 18:27:29 -0700

    Update main.c and sudoku_solve.c

--- rush01/main.bk.c
+++ rush01/main.bk.c
@@ -6,27 +6,36 @@
 /*   By: pfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 11:11:09 by pfu               #+#    #+#             */
-/*   Updated: 2018/03/25 18:11:22 by pfu              ###   ########.fr       */
+/*   Updated: 2018/03/25 16:52:23 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "fun/ft.h"
+int     sudoku_solve(int **num, int n);
 
-void	print_nbr(int a);
-int		sudoku_solve(int **num, int n);
+// For print nbr
+void	print_nbr(int a)
+{
+	char	b;
 
+	b = a + '0';
+	write(1, &b, 1);
+}
+
+//1.2
 int		is_legal(char c)
 {
 	if (c == '\0')
 		return (0);
-	else if (c >= '1' && c <= '9')
+	else if (c >= '0' && c <= '9')
 		return (1);
 	else if (c == '.')
 		return (1);
 	return (0);
 }
 
+// 1.1
 int		get_digi(char *str)
 {
 	int		i;
@@ -37,6 +46,7 @@ int		get_digi(char *str)
 	return (i);
 }
 
+// 1.
 int		**getnumber(char **argv)
 {
 	int		x;
@@ -45,10 +55,14 @@ int		**getnumber(char **argv)
 
 	x = 0;
 	num = (int **)malloc(sizeof(int *) * 9 + 1);
+	ft_putstr("The input is: \n");
 	while (x < 9)
 	{
 		if (get_digi(argv[x]) != 9)
+		{
+			printf("get_digi(argv[%d]) != 9 is %d\n", x, get_digi(argv[x]));
 			return (0);
+		}
 		num[x] = (int *)malloc(sizeof(int) * 9 + 1);
 		y = 0;
 		while (y < 9)
@@ -59,13 +73,19 @@ int		**getnumber(char **argv)
 				num[x][y] = argv[x][y] - '0';
 			else
 				return (0);
+			//
+			print_nbr(num[x][y]);
+			ft_putchar(' ');
 			y++;
 		}
+		ft_putchar('\n');
 		x++;
 	}
+	ft_putstr("-------------------\n");
 	return (num);
 }
 
+// 2.
 void	display_num_array(int **num)
 {
 	int		x;
@@ -93,16 +113,12 @@ int		main(int argc, char **argv)
 	int		**num;
 
 	if (argc != 10)
-	{
-		write(1, "Error\n", 6);
 		return (0);
-	}
 	num = getnumber(&argv[1]);
-	if (!num)
-		write(1, "Error\n", 6);
-	else if (sudoku_solve(num, 0))
+	//Use if... to run sudoku_solve() and get result!
+	if (sudoku_solve(num, 0))
 		display_num_array(num);
 	else
-		write(1, "Error\n", 6);
+		write(1, "Can't find answer!\nERROR!\n", 30);
 	return (0);
 }
--- rush01/sudoku_solve.bk.c
+++ rush01/sudoku_solve.bk.c
@@ -6,70 +6,66 @@
 /*   By: pfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/25 14:51:06 by pfu               #+#    #+#             */
-/*   Updated: 2018/03/25 18:22:00 by pfu              ###   ########.fr       */
+/*   Updated: 2018/03/25 16:50:22 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fun/ft.h"
-#include <unistd.h>
-
-void	print_nbr(int a)
-{
-	char	b;
-
-	b = a + '0';
-	write(1, &b, 1);
-}
 
 int		check_ans(int **num, int x, int y, int testans)
 {
 	int		i;
 	int		j;
+	int		box_x;
+	int		box_y;
 
 	i = 0;
-	while (i < 9)
+	while (i < 9)  // Check x and y's number, if have any same number then this ans is wrong!
 	{
 		if (num[i][y] == testans || num[x][i] == testans)
 			return (0);
 		i++;
 	}
-	i = 0;
+	i = 0;  // reset i to check 3*3 box
 	while (i < 3)
 	{
 		j = 0;
 		while (j < 3)
 		{
-			if (num[x - (x % 3) + i][y - (y % 3) + i] == testans)
+			box_x = (x - (x % 3) + i);
+			box_y = (y - (y % 3) + i);
+			if (num[box_x][box_y] == testans)
 				return (0);
 			j++;
 		}
 		i++;
 	}
-	return (1);
+	return (1);  // if is possible answer, return 0
 }
+
 
 int		sudoku_solve(int **num, int value)
 {
-	int		i;
+	int		i;  // the number we try to write.
 	int		x;
 	int		y;
 
-	i = 1;
-	x = value / 9;
-	y = value % 9;
-	if (value == 81)
-		return (1);
-	if (num[x][y] != 0)
+	i = 1;  // set the init for test
+	x = value / 9;  //to know which row we are.
+	y = value % 9;  // to know which column we are.
+	if (value == 81)  // If we succus for checking all place!
+		return (1);  // return 1 to print out result!
+	if (num[x][y] != 0)  // It's mean ... this place don't need to be solve, go NEXT.
 		return (sudoku_solve(num, value + 1));
-	while (i < 10)
+	while (i < 10)  // In sudoku, i just in 1 ~ 9.
 	{
 		if (check_ans(num, x, y, i))
 		{
-			num[x][y] = i;
+			num[x][y] = i;  // if is possible ans, write it down and carry on until all done or wrong
 			if (sudoku_solve(num, value + 1))
-				return (1);
+				return (1); // if ALL DONE, return 1. Then go back to main to print ANSWER.
 			else
-				num[x][y] = 0;
+				num[x][y] = 0;  // If go wrong, reset all testand to 0.
 		}
 		i++;
 	}
