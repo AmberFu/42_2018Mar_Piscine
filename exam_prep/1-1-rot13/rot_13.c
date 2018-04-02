e6f251cdc76a97027afd6dc62c0d6288c2030877
Po-ying Fu <pfu@e1z3r9p25.42.us.org>
2018-04-02 01:44:01 -0700

    add rot_13.c subject.en.txt

---
+++ exam_prep/1-1-rot13/rot_13.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rot_13.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/02 00:07:13 by pfu               #+#    #+#             */
+/*   Updated: 2018/04/02 01:42:53 by pfu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	lower_rot13(char c)
+{
+	if ((c + 13) <= 'z')
+	{
+		c = c + 13;
+		ft_putchar(c);
+	}
+	else if ((c + 13) > 'z')
+	{
+		c = c + 13 - 'z' + 'a' - 1;
+		ft_putchar(c);
+	}
+}
+
+void	upper_rot13(char c)
+{
+	if ((c + 13) <= 'Z')
+	{
+		c = c + 13;
+		ft_putchar(c);
+	}
+	else if ((c + 13) > 'Z')
+	{
+		c = c - 'Z' + 'A' - 1;
+		ft_putchar(c);
+	}
+}
+
+void	rot13(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			lower_rot13(*str);
+		else if (*str >= 'A' && *str <= 'Z')
+			upper_rot13(*str);
+		else
+			ft_putchar(*str);
+		str++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	rot13(av[1]);
+	return (0);
+}
